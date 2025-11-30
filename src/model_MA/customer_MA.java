4e0a3e2e39644d6922d537b23180488625e5e1d7
Esraa240 <sreya854@gmail.com>
2025-11-30 23:08:27 +0200

    تعديل

--- src/model_MA/customer_MA.java
+++ src/model_MA/customer_MA.java
@@ -1,30 +1,40 @@
-package model_MA;                          
+package model_MA;
 
-public class customer_MA {                   
-    private int customeridt;              
-    private String customernamet;          
-    private String phonet;                 
+public class customer_MA {
+
+    private int customeridt;
+    private String customernamet;
+    private String phonet;
 
     public customer_MA(int customeridt, String customernamet, String phonet) {
-        // Constructor ينفذ لما نديرو new customer_MA(...)
-        this.customeridt = customeridt;    
+        this.customeridt = customeridt;
         this.customernamet = customernamet;
-        this.phonet = phonet;             
+        this.phonet = phonet;
     }
-    public int getCustomeridt() {          
-        return customeridt;                
+
+    public int getCustomeridt() {
+        return customeridt;
     }
-    public String getCustomernamet() {     
-        return customernamet;              
+
+    public String getCustomernamet() {
+        return customernamet;
     }
-    public String getPhonet() {            
-        return phonet;                     
+
+    public String getPhonet() {
+        return phonet;
     }
-         
+
+    public void setCustomernamet(String customernamet) {
+        this.customernamet = customernamet;
+    }
+    public void setPhonet(String phonet) {
+        this.phonet = phonet;
+    }
+
     @Override
-    public String toString() {  //ترجع نص يمثل الزبون            
-        return "Customer ID: " + customeridt
-               + ", Name: " + customernamet
-               + ", Phone: " + phonet;
+    public String toString() {
+        return "Customer ID: " + customeridt +
+                ", Name: " + customernamet +
+                ", Phone: " + phonet;
     }
 }
